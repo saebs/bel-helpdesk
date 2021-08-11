41c8d496249118ddbcaea155453ea0d39a48a3ec
sabelo <ntabenism@gmail.com>
2021-08-11 13:51:04 +0200

    no overrides

--- BelinaHelpDesk/Data/BelinaHelpDeskContext.cs
+++ BelinaHelpDesk/Data/BelinaHelpDeskContext.cs
@@ -30,39 +30,39 @@ namespace BelinaHelpDesk.Data
 
 
 
-            modelBuilder.Entity<HelpDeskTicket>(entity =>
-            {
-                entity.Property(e => e.TicketDate).HasColumnType("datetime");
-
-                entity.Property(e => e.TicketDescription).IsRequired();
-
-                entity.Property(e => e.TicketGuid)
-                    .IsRequired()
-                    .HasMaxLength(500)
-                    .HasColumnName("TicketGUID");
-
-                entity.Property(e => e.TicketRequesterEmail)
-                    .IsRequired()
-                    .HasMaxLength(500);
-
-                entity.Property(e => e.TicketStatus)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<HelpDeskTicketDetail>(entity =>
-            {
-                entity.Property(e => e.TicketDescription).IsRequired();
-
-                entity.Property(e => e.TicketDetailDate).HasColumnType("datetime");
-
-                entity.HasOne(d => d.HelpDeskTicket)
-                    .WithMany(p => p.HelpDeskTicketDetails)
-                    .HasForeignKey(d => d.HelpDeskTicketId)
-                    .HasConstraintName("FK_HelpDeskTicketDetails_HelpDeskTickets");
-            });
-
-            OnModelCreatingPartial(modelBuilder);
+            // modelBuilder.Entity<HelpDeskTicket>(entity =>
+            // {
+            //     entity.Property(e => e.TicketDate).HasColumnType("datetime");
+            //
+            //     entity.Property(e => e.TicketDescription).IsRequired();
+            //
+            //     entity.Property(e => e.TicketGuid)
+            //         .IsRequired()
+            //         .HasMaxLength(500)
+            //         .HasColumnName("TicketGUID");
+            //
+            //     entity.Property(e => e.TicketRequesterEmail)
+            //         .IsRequired()
+            //         .HasMaxLength(500);
+            //
+            //     entity.Property(e => e.TicketStatus)
+            //         .IsRequired()
+            //         .HasMaxLength(50);
+            // });
+            //
+            // modelBuilder.Entity<HelpDeskTicketDetail>(entity =>
+            // {
+            //     entity.Property(e => e.TicketDescription).IsRequired();
+            //
+            //     entity.Property(e => e.TicketDetailDate).HasColumnType("datetime");
+            //
+            //     entity.HasOne(d => d.HelpDeskTicket)
+            //         .WithMany(p => p.HelpDeskTicketDetails)
+            //         .HasForeignKey(d => d.HelpDeskTicketId)
+            //         .HasConstraintName("FK_HelpDeskTicketDetails_HelpDeskTickets");
+            // });
+            //
+            // OnModelCreatingPartial(modelBuilder);
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
