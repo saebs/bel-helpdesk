09eae33aa01f7896aedd3f1e34d106f69d229a0e
sabelo <ntabenism@gmail.com>
2021-08-11 13:24:42 +0200

    Annotate Ticket class constraints

--- BelinaHelpDesk/Data/Migrations/20210811111613_AnnotateTicketClass.Designer.cs
+++ BelinaHelpDesk/Data/Migrations/20210811111613_AnnotateTicketClass.Designer.cs
@@ -4,14 +4,16 @@ using BelinaHelpDesk.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BelinaHelpDesk.Data.Migrations
 {
     [DbContext(typeof(BelinaHelpDeskContext))]
-    partial class BelinaHelpDeskContextModelSnapshot : ModelSnapshot
+    [Migration("20210811111613_AnnotateTicketClass")]
+    partial class AnnotateTicketClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BelinaHelpDesk/Data/Migrations/20210811111613_AnnotateTicketClass.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BelinaHelpDesk.Data.Migrations
+{
+    public partial class AnnotateTicketClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TicketDescription",
+                table: "HelpDeskTickets",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TicketDescription",
+                table: "HelpDeskTickets",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
