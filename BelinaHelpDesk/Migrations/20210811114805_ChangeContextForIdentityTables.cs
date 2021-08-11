a0e8f9fd8e20a824e421e2e1dbd743b1469f46c2
sabelo <ntabenism@gmail.com>
2021-08-11 13:52:15 +0200

    Change context

---
+++ BelinaHelpDesk/Migrations/20210811114805_ChangeContextForIdentityTables.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BelinaHelpDesk.Migrations
+{
+    public partial class ChangeContextForIdentityTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
