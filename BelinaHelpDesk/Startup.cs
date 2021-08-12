8cefadce3053d647e7fe04d9ff9847b03d1f7c73
sabelo <ntabenism@gmail.com>
2021-08-12 17:28:49 +0200

    synfusion license registration etc

--- BelinaHelpDesk/Startup.cs
+++ BelinaHelpDesk/Startup.cs
@@ -15,6 +15,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BelinaHelpDesk.Areas.Identity;
 using BelinaHelpDesk.Data;
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.Internal;
 
 namespace BelinaHelpDesk
 {
@@ -39,6 +41,7 @@ namespace BelinaHelpDesk
                 .AddEntityFrameworkStores<BelinaHelpDeskContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSyncfusionBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddScoped<BelinaHelpDeskService>();
@@ -47,6 +50,11 @@ namespace BelinaHelpDesk
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            Syncfusion
+                .Licensing
+                .SyncfusionLicenseProvider
+                .RegisterLicense
+                    ("NDg1MjA5QDMxMzkyZTMyMmUzMFZSZDI3QTdFTWVsdU1aS2pXYVV3cGhaUUFjZ2YvVlNEWEhJSE8rUXNGeGc9;NDg1MjEwQDMxMzkyZTMyMmUzMGkrN2NuSnhzVUF6V2tHZ1F6OTJZU2Q2czMvZHFCUGdTMnpMWXhhcW1nWmM9;NDg1MjExQDMxMzkyZTMyMmUzMEUyQlhuR3lyOEtHeC8xTVIzeEJLeGFhdzJmeG9rRFJKdVQ5U1BCblJzbnc9;NDg1MjEyQDMxMzkyZTMyMmUzMFIrNzlscDYrd2IxRDlZQ1Uray9TN1gwTmNtMXZvMWIzZWJ2MmNieHpsYmM9;NDg1MjEzQDMxMzkyZTMyMmUzMGlnbXNLRDV3SGpvU2FuWU9aeDJWYjBXbHBNRzcwWGZxOXAwemxwVHVHcm89;NDg1MjE0QDMxMzkyZTMyMmUzMGxEVEE5ajNZQ25pa1ZueUQ1RzBRcndORFlxMHBMd3pxSk5Md1B1VXZXTEE9;NDg1MjE1QDMxMzkyZTMyMmUzME03RHBKOU1QeUwybWwxcTd0WVozdVVvZkJWK0R0TXNQa3FQM2FxMytua1k9;NDg1MjE2QDMxMzkyZTMyMmUzMG9ZbjJaemlJNkttWjJPQlFIWisxRXYyR0loZTYySjE5NDhnbHNlb1hZd2c9;NDg1MjE3QDMxMzkyZTMyMmUzMElXRWZuNnJpSVJPMmFXN3BtQkJZK0FjVnNzK1hJd3dDMG44TjBvQUxJdDg9;NDg1MjE4QDMxMzkyZTMyMmUzMGJsaVQ3Z1VTN0haSVJyYlV0ZEoxQUsycWMzbjQ1SUZJa25JN0Z6SWJsV1U9");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
