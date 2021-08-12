8e43e3dca76f300463c01223b239ba93ffd9c8cc
sabelo <ntabenism@gmail.com>
2021-08-12 13:44:58 +0200

    foo

---
+++ BelinaHelpDesk/Data/Migrations/20210812103445_Checking.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BelinaHelpDesk.Data.Migrations
+{
+    public partial class Checking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
