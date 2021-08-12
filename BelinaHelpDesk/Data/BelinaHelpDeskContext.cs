955fec6fc2e62ecd8078ea21a0700f063a6bec8e
sabelo <ntabenism@gmail.com>
2021-08-12 17:39:10 +0200

    autogenerated migrations and domain specific ones

--- BelinaHelpDesk/Data/BelinaHelpDeskContext.cs
+++ BelinaHelpDesk/Data/BelinaHelpDeskContext.cs
@@ -10,6 +10,8 @@ namespace BelinaHelpDesk.Data
     public class BelinaHelpDeskContext : IdentityDbContext
     {
         public DbSet<HelpDeskTicket> HelpDeskTickets { get; set; }
+        public DbSet<HelpDeskTicketDetail> HelpDeskTicketDetails { get; set; }
+        public DbSet<HelpDeskStatus> HelpDeskStatuses { get; set; }
         public BelinaHelpDeskContext(DbContextOptions<BelinaHelpDeskContext> options)
             : base(options)
         {
